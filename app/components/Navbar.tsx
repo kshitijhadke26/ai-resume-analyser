f5510c2ad21b3d463531c17e9f38249203ce14c2
kshitijhadke26 <kshitijhadke26@gmail.com>
2025-08-12 01:50:52 +0530

    Create Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,15 +2,15 @@ import { Link } from "react-router";
 
 const Navbar = () => {
 	return (
-      <nav className="navbar">
-         <Link to="/">
-            <p className="text-2xl font-bold text-gradient">RESUNERADAR</p>
-         </Link>
-         <Link className="primary-button w-fit">
-            Upload Resume
-         </Link>
-      </nav>
-   )
+		<nav className="navbar">
+			<Link to="/">
+				<p className="text-2xl font-bold text-gradient">RESUNERADAR</p>
+			</Link>
+			<Link to="/upload" className="primary-button w-fit">
+				Upload Resume
+			</Link>
+		</nav>
+	);
 };
 
 export default Navbar;
