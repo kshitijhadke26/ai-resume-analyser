9be44a035e6a03e1da59e1fd0d71b1812c98d0ed
kshitijhadke26 <kshitijhadke26@gmail.com>
2025-09-17 19:05:28 +0530

    deployment problem

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  optimizeDeps: {
+    include: [
+      "zustand",
+      "react-dropzone",
+      "react-icons",
+      "pdfjs-dist"
+    ]
+  },
 });
